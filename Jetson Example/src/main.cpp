18e8031a437ed66ed84920039301c989451bf141
someMEXICANz <randylujano@gmail.com>
2025-07-04 06:01:11 +0200

    up

--- Jetson Example/src/main.cpp
+++ Jetson Example/src/main.cpp
@@ -3,18 +3,96 @@
 #include <chrono>
 
 #include <WS_UPS.h>
-// #include <IMU.h>
 #include <BrainComm.h>
-// #include <RobotPosition.h>
 #include <Model.h>
 #include <ObjectDetection.h>
 #include <Camera.h>
-// #include <FieldMapper.h>
+
 #include <BNO085.h>
 
 
 
 using namespace std;
+
+void printUPSdata(UPS &ups);
+
+
+int main() 
+{
+    UPS ups; // (threaded)
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    while (true) 
+    {
+   
+        if(ups.isRunning())
+            printUPSdata(ups);
+        std::this_thread::sleep_for(std::chrono::milliseconds(250));    
+    }
+    ups.stop();
+    return 0;
+}
+
+
+
+
+void printUPSdata(UPS &ups)
+{
+    UPSData ups_data = ups.getUPSData();
+
+    std::cerr << "--------------------------------------------" << std::endl;
+    // Display UPS data 
+    std::cout << "WAVESHARE UPS DATA" << std::endl;
+
+    std::cout << "Data Valid: " ;
+    if(ups_data.valid)
+        std::cout << "TRUE " << std::endl ;
+    else
+        std::cout << "FALSE " << std::endl ;
+
+    std::cerr << "Load Voltage:     " << ups_data.busVoltage  << " V" << std::endl;
+    std::cerr << "Shunt Voltage:    " << ups_data.shuntVoltage  << " mV" << std::endl;
+    std::cerr << "PSU Voltage:      " << (ups_data.busVoltage  + ups_data.shuntVoltage) << " V" << std::endl;
+    std::cerr << "Current:          " << (ups_data.current) << " mA" << std::endl;
+    std::cerr << "Power:            " << ups_data.power << " mW" << std::endl;
+    std::cerr << "Battery Level:    " << ups_data.batteryLevel << "%" << std::endl;
+
+    std::time_t time = std::chrono::system_clock::to_time_t(ups_data.timestamp);
+    std::tm* tm_now = std::localtime(&time);
+    // Print the formatted time                                              
+    std::cout << "--------------------------------------------" << std::endl;
+    std::cout << "Timestamp: " << std::put_time(tm_now, "%Y-%m-%d %H:%M:%S") << std::endl;
+    std::cout << "--------------------------------------------" << std::endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // void printIMUData(IMU &imu)
 // {
@@ -48,109 +126,76 @@ using namespace std;
 // }
 
 
-void printUPSdata(UPS &ups)
-{
-    UPSData ups_data = ups.getUPSData();
-
-    std::cerr << "--------------------------------------------" << std::endl;
-    // Display UPS data 
-    std::cout << "WAVESHARE UPS DATA" << std::endl;
-
-    std::cout << "Data Valid: " ;
-    if(ups_data.valid)
-        std::cout << "TRUE " << std::endl ;
-    else
-        std::cout << "FALSE " << std::endl ;
-
-    std::cerr << "Load Voltage:     " << ups_data.busVoltage  << " V" << std::endl;
-    std::cerr << "Shunt Voltage:    " << ups_data.shuntVoltage  << " mV" << std::endl;
-    std::cerr << "PSU Voltage:      " << (ups_data.busVoltage  + ups_data.shuntVoltage) << " V" << std::endl;
-    std::cerr << "Current:          " << (ups_data.current) << " mA" << std::endl;
-    std::cerr << "Power:            " << ups_data.power << " mW" << std::endl;
-    std::cerr << "Battery Level:    " << ups_data.batteryLevel << "%" << std::endl;
-
-    std::time_t time = std::chrono::system_clock::to_time_t(ups_data.timestamp);
-    std::tm* tm_now = std::localtime(&time);
-    // Print the formatted time                                              
-    std::cout << "--------------------------------------------" << std::endl;
-    std::cout << "Timestamp: " << std::put_time(tm_now, "%Y-%m-%d %H:%M:%S") << std::endl;
-    std::cout << "--------------------------------------------" << std::endl;
-}
 
 
-int main() 
-{
-    boost::asio::io_service myService;
 
-    UPS ups; // (threaded)
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    // IMU imu; // (threaded)
-    // std::this_thread::sleep_for(std::chrono::seconds(1));
+// int main() 
+// {
+//     boost::asio::io_service myService;
+
+//     UPS ups; // (threaded)
+//     std::this_thread::sleep_for(std::chrono::seconds(1));
+//     // IMU imu; // (threaded)
+//     // std::this_thread::sleep_for(std::chrono::seconds(1));
     
-    Camera camera; // (threaded)
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    // Brain::BrainComm brain(myService); // (threaded)
-    // std::this_thread::sleep_for(std::chrono::seconds(1));
+//     Camera camera; // (threaded)
+//     std::this_thread::sleep_for(std::chrono::seconds(1));
+//     // Brain::BrainComm brain(myService); // (threaded)
+//     // std::this_thread::sleep_for(std::chrono::seconds(1));
 
-    // RobotPosition robotPosition(brain, imu, myService); // (threaded) 
-    // std::this_thread::sleep_for(std::chrono::seconds(1));
+//     // RobotPosition robotPosition(brain, imu, myService); // (threaded) 
+//     // std::this_thread::sleep_for(std::chrono::seconds(1));
 
-    // Model model;
-    // ObjectDetection objdet;
-    // std::vector<DetectedObject> Detections;
-    // std::this_thread::sleep_for(std::chrono::seconds(1));
+//     // Model model;
+//     // ObjectDetection objdet;
+//     // std::vector<DetectedObject> Detections;
+//     // std::this_thread::sleep_for(std::chrono::seconds(1));
     
-    // //FieldMapper mapper(camera); //, robotPosition); // (threaded)
-    // std::this_thread::sleep_for(std::chrono::seconds(1));
+//     // //FieldMapper mapper(camera); //, robotPosition); // (threaded)
+//     // std::this_thread::sleep_for(std::chrono::seconds(1));
 
    
-    std::cerr << endl;
+//     std::cerr << endl;
 
-    // imu.calibrateAccelerometer();
+//     // imu.calibrateAccelerometer();
     
  
 
 
     
 
-    while (true) 
-    {
-        // std::cerr << "Camera FPS: " << camera.getFPS() << std::endl;
-        // //std::cerr << "Mapper PPS: " << mapper.getPPS() << std::endl;
+//     while (true) 
+//     {
+//         // std::cerr << "Camera FPS: " << camera.getFPS() << std::endl;
+//         // //std::cerr << "Mapper PPS: " << mapper.getPPS() << std::endl;
 
-        // if(camera.getInferFrame(model.inferInput))
-        // {
-        //     model.runInference();
-        //     Detections = objdet.decodeOutputs(model.inferOutput1, model.inferOutput2);
-        //     std::cerr << "Found " << Detections.size() << " detected objects" << std::endl;;
-        // }
-        // if(imu.isRunning())
-        //     printIMUData(imu);
-        if(ups.isRunning())
-            printUPSdata(ups);
+//         // if(camera.getInferFrame(model.inferInput))
+//         // {
+//         //     model.runInference();
+//         //     Detections = objdet.decodeOutputs(model.inferOutput1, model.inferOutput2);
+//         //     std::cerr << "Found " << Detections.size() << " detected objects" << std::endl;;
+//         // }
+//         // if(imu.isRunning())
+//         //     printIMUData(imu);
+//         if(ups.isRunning())
+//             printUPSdata(ups);
             
-        // if(brain.isConnected() && brain.isRunning())
-        // {
-        //     brain.setJetsonBattery(ups.getBatteryPercentage());
-        // }
+//         // if(brain.isConnected() && brain.isRunning())
+//         // {
+//         //     brain.setJetsonBattery(ups.getBatteryPercentage());
+//         // }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(250));    
-    }
-    //mapper.stop();
-    // camera.stop();
-    // robotPosition.stop();
-    // brain.stop();
-    // imu.stop();
-    ups.stop();
+//         std::this_thread::sleep_for(std::chrono::milliseconds(250));    
+//     }
+//     //mapper.stop();
+//     // camera.stop();
+//     // robotPosition.stop();
+//     // brain.stop();
+//     // imu.stop();
+//     ups.stop();
     
     
-    return 0;
-}
-
-
-
-
-
-
+//     return 0;
+// }
 
 
